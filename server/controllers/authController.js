d9d2ea8b043d0ff8b26c42df70b7f6ada58d83bc
Michał <michal.bielawski@hotmail.com>
2025-06-04 11:39:49 +0200

    created login method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -47,3 +47,25 @@ exports.signup = async (req, res, next) => {
     })
     createAndSendToken(newUser, 201, 400, res)
 }
+
+exports.login = async (req, res, next) => {
+    // getting credentials from object's body
+    const {email, password} = req.body
+    // 1) Check if email and password exist
+    if(!email || !password){
+        return next(new AppError("Please provide email and password!", 400))
+    }
+    // 2) Check if user exists and password is correct
+    const user = await User.findOne({email}).select("+password")
+        // using the password comparing method declared in user model 
+    if(!user || !(await user.correctPassword(password, user.password))){
+        res.status(401).json({
+            status: "Failed",
+            message: "Wrong email or password"
+        })
+    }
+
+    // 3) If everything is okay, send the token to user
+    createAndSendToken(user, 201, 400, res)
+
+}
