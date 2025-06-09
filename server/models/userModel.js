3027d725a03752ee66eaee3bce26de86c8c56060
Michał <michal.bielawski@hotmail.com>
2025-06-09 12:08:51 +0200

    refactoring user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -44,6 +44,27 @@ const userSchema = new mongoose.Schema({
     passwordResetExpires: String,
     userExpenses: Array
 })
+userSchema.pre("save", async function(next){
+    // Only run this function if password was modified
+    if(!this.isModified("password")){
+        return next();
+    };
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    // Delete password confirm field
+    this.passwordConfirm = undefined;
+    next();
+})
+
+userSchema.pre("save", function(next){
+    if(!this.isModified('password') || this.isNew ){
+        return next();
+    }
+    /// substracting 1 milisecond from the date so that the token is always created after the password has been changed
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+
+})
 
 const User = mongoose.model("User", userSchema)
 
